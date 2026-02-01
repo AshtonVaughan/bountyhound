07a1a48514aa0da74442bb1418cbaad64bec3955
AshtonVaughan <ashtonluca@gmail.com>
2026-02-02 03:17:00 +1000

    feat: add Pydantic data models for targets, subdomains, findings

---
+++ bountyhound/storage/__init__.py
@@ -0,0 +1,5 @@
+"""Storage package for database operations."""
+
+from bountyhound.storage.models import Target, Subdomain, Port, Finding, Run
+
+__all__ = ["Target", "Subdomain", "Port", "Finding", "Run"]
